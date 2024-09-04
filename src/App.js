cc671b3939932a9682dbb69b82af876e3a921d08
Eylül Özatman <74192529+eylulozatman@users.noreply.github.com>
2024-09-05 00:56:47 +0300

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './homepage'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './homepage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/rick-morty-eylul">
       <div className="App">
         <Routes>
           <Route path="/" element={<Homepage />} />
