36d4f6cd8913dc3a66fe73e1ea5722ac506a7bf5
Eylül Özatman <74192529+eylulozatman@users.noreply.github.com>
2024-09-05 00:49:15 +0300

    browser route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './homepage';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './Homepage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-    
         <Routes>
-          <Route path="/" element={<Homepage />} /> 
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </div>
     </Router>
