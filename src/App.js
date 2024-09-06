0dc660015c5b3cda04857ea6aee17384d9e5a6ee
Eylül Özatman <74192529+eylulozatman@users.noreply.github.com>
2024-09-06 11:23:27 +0300

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './homepage';
-
-
 import './App.css';
 function App() {
   return (
     <Router >
-
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
-    
     </Router>
   );
 }
